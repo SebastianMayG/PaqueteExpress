aac8e3dca7d1accc7d6f7a72d62d543442cdf1a3
SebastianMayG <al064488@uacam.mx>
2025-11-08 12:00:12 -0600

    fix: url de github pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'https://SebastianMayG.github.io/PaqueteExpress/',
+  base: '/PaqueteExpress/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
